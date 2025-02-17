9911cef577ef51fc6e4099454fbc84ccf0083be7
Gartland-R2 <Gartland-R2>
2025-02-17 20:12:27 +0000

    first commit

---
+++ src/main/java/com/siopa/siopa_auth/SiopaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.siopa.siopa_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiopaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiopaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siopa/siopa_auth/SiopaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siopa.siopa_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiopaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
